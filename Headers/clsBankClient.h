4c909b3b27b81d1c4ff21b88ddb438f2ff95d1fd
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-06-14 20:57:15 +0100

    fix complete get set properities of clsPerson and clsBankClient classes with function IsEmpty for clsBankClient class

--- Headers/clsBankClient.h
+++ Headers/clsBankClient.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
 
 #include "clsPerson.h"
 
@@ -9,32 +13,37 @@ using namespace std;
 class clsBankClient : public clsPerson
 {
 private:
+
+	enum enMode {EmptyMode = 0, UpdateMode = 1};
+
+	enMode _Mode;
+
 	string _AccountNumber;
 	string _PinCode;
 	float _Balance;
 
+	clsBankClient _ConvertLineToClient(const string& Line)
+	{
+
+	}
+
 public:
 
-	clsBankClient(const string& FirstName, const string& LastName, const string& Email, const string& Phone, const string AccountNumber,
-		const string& PinCode, float Balance) : clsPerson(FirstName, LastName, Email, Phone)
+	clsBankClient(enMode Mode, const string& FirstName, const string& LastName, const string& Email, const string& Phone
+		, const string AccountNumber, const string& PinCode, float Balance) : clsPerson(FirstName, LastName, Email, Phone)
 	{
+
+		_Mode = Mode;
 		_AccountNumber = AccountNumber;
 		_PinCode = PinCode;
 		_Balance = Balance;
 
 	}
 
-	void SetAccountNumber(const string& AccountNumber)
-	{
-		_AccountNumber = AccountNumber;
-	}
-
 	string GetAccountNumber()
 	{
 		return _AccountNumber;
 	}
-
-	__declspec(property(get = GetAccountNumber, put = SetAccountNumber)) string AccountNumber;
 
 	void SetPinCode(const string& PinCode)
 	{
@@ -60,7 +69,34 @@ public:
 
 	__declspec(property(get = GetBalance, put = SetBalance)) string Balance;
 
+	bool IsEmpty()
+	{
+		return _Mode == EmptyMode;
+	}
 
+	clsBankClient Find(const string& AccountNumber, const string& FileName)
+	{
+		fstream File;
+
+		File.open(FileName, ios::in);
+
+		if (File.is_open())
+		{
+			string Line = "";
+
+			while (getline(File, Line))
+			{
+				if()
+			}
+
+
+			File.close();
+
+		}
+
+
+
+	}
 
 
 
