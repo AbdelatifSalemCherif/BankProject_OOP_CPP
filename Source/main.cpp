8c45e11528d8b0db89e3262b8ca3c900b1d34511
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-03 18:51:49 +0100

    Correct an error in main, error of ReadNumber changed to ReadFloat function

--- Source/main.cpp
+++ Source/main.cpp
@@ -14,7 +14,7 @@ void ReadClientInfo(clsBankClient& Client)
 	Client.Email = clsInputSettings::ReadString("\nPlease enter Email : ");
 	Client.Phone = clsInputSettings::ReadString("\nPlease enter Phone : ");
 	Client.PinCode = clsInputSettings::ReadString("\nPlease enter Pin Code : ");
-	Client.Balance = clsInputSettings::ReadNumber((float)0, "\nPlease enter Balance : ");
+	Client.Balance = clsInputSettings::ReadFloat("\nPlease enter Balance : ");
 
 }
 
