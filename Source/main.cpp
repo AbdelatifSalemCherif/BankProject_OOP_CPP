05d2021f5d1f7cb0534646a281e44b2f5a47cec7
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-08 16:31:52 +0100

    Add to the main PrintClientLineofTotalBalanceTable and ShowTotalBalance functions

--- Source/main.cpp
+++ Source/main.cpp
@@ -176,18 +176,56 @@ void ShowClientsList()
 
 void PrintClientLineOfTotalBalanceTable(const clsBankClient& Client)
 {
-
+	cout << "| " << left << setw(30) << Client.GetAccountNumber();
+	cout << "| " << left << setw(30) << Client.FirstName + " " + Client.LastName;
+	cout << "| " << left << setw(30) << to_string(Client.Balance) << endl;
 }
 
 void ShowTotalBalance()
 {
+	vector <clsBankClient> vClients = clsBankClient::GetClientsList(FileName, Separator);
+
+
+	cout << "\n\t\t\t\t\t\tTotal Balance (" << vClients.size() << ") Clients" << endl;
+
+	cout << "----------------------------------------------------------------------------------------------------------------"
+		"-------" << endl;
+
+	cout << "| " << left << setw(30) << "Account Number";
+	cout << "| " << left << setw(30) << "Client Name";
+	cout << "| " << left << setw(30) << "Balance" << endl;
+
+	cout << "----------------------------------------------------------------------------------------------------------------"
+		"-------" << endl;
+
+	if (vClients.size() == 0)
+	{
+		cout << "\t\t\t\tNo Clients Available In System !" << endl;
+	}
+	else
+	{
+		for (const clsBankClient& Client : vClients)
+		{
+			PrintClientLineOfTotalBalanceTable(Client);
+		}
+	}
+
+	cout << "----------------------------------------------------------------------------------------------------------------"
+		"-------" << endl;
+
+	const double TotalBalace = clsBankClient::GetTotalBalance(FileName, Separator);
+
+	cout << "\t\t\tTotal Balance = " << TotalBalace << endl;
+
+	cout << "\t\t\t\t" << clsUtility::GetNumberText(TotalBalace) << endl;
+
 
 }
 
 int main()
 {
 	
-	cout << clsUtility::GetNumberText(1471482) << endl;
+	ShowTotalBalance();
 
 
 
