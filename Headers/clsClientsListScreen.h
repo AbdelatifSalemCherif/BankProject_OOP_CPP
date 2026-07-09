b3c9317f34d4cc997a3433f9e49274708232a649
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-09 18:22:49 +0100

    Add clsClientsListScreen.h file to Headers file

---
+++ Headers/clsClientsListScreen.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+#include "clsScreen.h"
+
+using namespace std;
+
+class clsClientsListScreen : protected clsScreen
+{
+
+
+
+
+
+
+
+
+
+
+};
