983c79cfd6b23dfd124a1216ad2c27e39d9a6e8c
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-11 19:09:04 +0100

    Add _PrintClientCard method to clsFindClientScreen class

--- Headers/clsFindClientScreen.h
+++ Headers/clsFindClientScreen.h
@@ -11,6 +11,24 @@ using namespace std;
 class clsFindClientScreen : protected clsScreen
 {
 
+private:
+
+	static void _PrintClientCard(const clsBankClient& Client)
+	{
+		cout << "\nClient Card :" << endl;
+		cout << "------------------------------------------------" << endl;
+		cout << "First Name :" << Client.FirstName << endl;
+		cout << "Last Name  :" << Client.LastName << endl;
+		cout << "Full Name  :" << Client.FirstName + " " + Client.LastName << endl;
+		cout << "Email      :" << Client.Email << endl;
+		cout << "Phone      :" << Client.Phone << endl;
+		cout << "Acc.Number :" << Client.GetAccountNumber() << endl;
+		cout << "Password   :" << Client.PinCode << endl;
+		cout << "Balance    :" << Client.Balance << endl;
+		cout << "------------------------------------------------" << endl;
+
+	}
+
 
 
 
