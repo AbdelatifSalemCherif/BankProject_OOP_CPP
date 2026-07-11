c512b6de6e83665ac9cd7474e017ffd4776ba6a8
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-11 20:07:21 +0100

    Add enTransactionOptions enum to clsTransactionsScreen class

--- Headers/clsTransactionsScreen.h
+++ Headers/clsTransactionsScreen.h
@@ -11,12 +11,20 @@ class clsTransactionsScreen : protected clsScreen
 
 private:
 
+	enum enTransactionOptions { eDeposite = 1, eWithdraw = 2, eTotalBalance = 3, eExit = 4};
 
 
 
 public:
 
+	static void ShowTransactionsScreen(const string& FileName, const string& Separator)
+	{
 
+
+
+
+
+	}
 
 
 
