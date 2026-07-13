e105ca6196dbed2837a125fc531b4ec1420f0b8c
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-13 20:05:09 +0100

    Add _PrintClientCard method to clsWithdrawScreen class

--- Headers/clsWithdrawScreen.h
+++ Headers/clsWithdrawScreen.h
@@ -13,8 +13,29 @@ class clsWithdrawScreen : protected clsScreen
 
 private:
 
+	static void _PrintClientCard(const clsBankClient& Client)
+	{
+		cout << "\nClient Card :" << endl;
+		cout << "------------------------------------------------" << endl;
+		cout << "First Name :" << Client.FirstName << endl;
+		cout << "Last Name  :" << Client.LastName << endl;
+		cout << "Full Name  :" << Client.FirstName + " " + Client.LastName << endl;
+		cout << "Email      :" << Client.Email << endl;
+		cout << "Phone      :" << Client.Phone << endl;
+		cout << "Acc.Number :" << Client.GetAccountNumber() << endl;
+		cout << "Password   :" << Client.PinCode << endl;
+		cout << "Balance    :" << Client.Balance << endl;
+		cout << "------------------------------------------------" << endl;
+
+	}
+
 
 public:
+
+
+
+
+
 
 
 };
