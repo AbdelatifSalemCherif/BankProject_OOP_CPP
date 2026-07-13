3326b7c22302c50a48d26bd115a88db6da846360
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-13 20:03:41 +0100

    Add clsWithdraw.h file to Headers file

---
+++ Headers/clsWithdrawScreen.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+
+#include "clsScreen.h"
+#include "clsBankClient.h"
+#include "clsInputSettings.h"
+
+using namespace std;
+
+class clsWithdrawScreen : protected clsScreen
+{
+
+private:
+
+
+public:
+
+
+};
