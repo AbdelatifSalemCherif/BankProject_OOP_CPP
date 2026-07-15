7ed81876e35b506e170438baa06e532af8a81dd0
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-15 18:35:29 +0100

    Add clsManageUsersScreen.h file to Headers file

---
+++ Headers/clsManageUsersScreen.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+
+class clsManageUsersScreen : protected clsScreen
+{
+
+private:
+
+
+
+
+public:
+
+
+
+};
