a802133fdb3d085c86e51ff8d8188b78c036e071
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-15 20:04:34 +0100

    Add clsBankUser.h file to Headers file

---
+++ Headers/clsBankUser.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+
+#include "clsPerson.h"
+#include "clsString.h"
+
+using namespace std;
+
+class clsBankUser : public clsPerson
+{
+
+private:
+
+
+
+public:
+
+
+
+
+};
+
+
