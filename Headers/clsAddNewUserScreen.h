64ff7b37cec2d33bee8061b2f7f3c05b689e5ae5
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-16 19:31:55 +0100

    Add clsAddNewUserScreen.h file to Headers dument file

---
+++ Headers/clsAddNewUserScreen.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+#include "clsBankUser.h"
+
+using namespace std;
+
+class clsAddNewUserScreen : protected clsScreen
+{
+
+private:
+
+
+
+
+public:
+
+
+
+};
+
+
