e9c0c7df2b754db68fdc11980acbd45536109605
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-16 20:31:39 +0100

    Add clsUpdateUserScreen.h file to Headers document file

---
+++ Headers/clsUpdateUserScreen.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+#include "clsBankUser.h"
+
+using namespace std;
+
+class clsUpdateUserScreen : protected clsScreen
+{
+
+private:
+
+
+
+public:
+
+
+
+
+};
