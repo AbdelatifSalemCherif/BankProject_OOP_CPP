b5b50cfdc469c2caf82f5828bb76eab7926b4c41
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-16 20:56:17 +0100

    Add clsFindUserScreen.h file to Headers document file

---
+++ Headers/clsFindUserScreen.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+#include "clsBankUser.h"
+
+using namespace std;
+
+class clsFindUserScreen : protected clsScreen
+{
+
+private:
+
+
+
+public:
+
+
+
+};
