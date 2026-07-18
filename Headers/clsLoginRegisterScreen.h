e7cf286508d2f7325baf11755599906804e759a9
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-18 17:46:24 +0100

    Add clsLoginRegisterScreen.h file to Headers document file

---
+++ Headers/clsLoginRegisterScreen.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+
+using namespace std;
+
+
+class clsLoginRegisterScreen : protected clsScreen
+{
+
+public:
+
+
+
+
+
+private:
+
+
+};
+
+
+
