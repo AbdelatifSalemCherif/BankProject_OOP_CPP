e017df79761ba149889e4706aefab1eba7762708
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-19 21:43:44 +0100

    Add clsTransferRegisterScreen.h file to Headers document file

---
+++ Headers/clsTransferRegisterScreen.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+#include "clsBankClient.h"
+
+using namespace std;
+
+class clsTransferRegisterScreen : protected clsScreen
+{
+
+private:
+
+
+
+
+
+
+public:
+
+
+
+
+
+};
