3f7996a00332ce8b663f381d9c5f90893dcb0cfa
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-19 17:59:06 +0100

    Add 2 overload of _ReadClient method and ShowTransferScreen method to clsTransferScreen class

--- Headers/clsTransferScreen.h
+++ Headers/clsTransferScreen.h
@@ -24,8 +24,92 @@ private:
 
 	}
 
+	static clsBankClient _ReadClient(const string& MessageToUser, const string& ErrorMessage)
+	{
+		clsBankClient Client = clsBankClient::Find(clsInputSettings::ReadString(MessageToUser));
+
+		while (Client.IsEmpty())
+		{
+			Client = clsBankClient::Find(clsInputSettings::ReadString(ErrorMessage));
+		}
+
+		return Client;
+	}
+
+	static clsBankClient _ReadClient(const clsBankClient& ExptionClient, const string& MessageToUser, const string& ErrorMessage)
+	{
+
+	
+		clsBankClient Client = clsBankClient::Find(clsInputSettings::ReadString(MessageToUser));
+
+		while (Client.IsEmpty() || Client.GetAccountNumber() == ExptionClient.GetAccountNumber())
+		{
+			Client = clsBankClient::Find(clsInputSettings::ReadString(ErrorMessage));
+		}
+
+		return Client;
+	}
+
+
 
 public:
+
+	static void ShowTransferScreen()
+	{
+
+		_DrawScreenHeader("Transfer Screen");
+
+		clsBankClient ClientTransferFrom = _ReadClient("\nPlease Enter Account Number To Transfer From : "
+			, "\nAccount Number Isn\'t Correct, Please Enter Another One : ");
+
+		_PrintClientCard(ClientTransferFrom);
+
+
+		clsBankClient ClientTransferTo = _ReadClient(ClientTransferFrom, "\nPlease Enter Account Number To Transfer To : ",
+			"\nAccount Number Isn\'t Correct, Or You Entered The Same Account Number To Transform From, Please Enter Another One : ");
+
+		_PrintClientCard(ClientTransferTo);
+
+
+		float Amount = clsInputSettings::ReadFloatInRange(0, ClientTransferFrom.Balance,
+			"\n\nPlease Enter Transfer Amount ? It Must Be Less Than " + to_string(ClientTransferFrom.Balance));
+
+
+		if (clsInputSettings::ReadYesOrNo("\n\nAre You Sure You Want To Perform This Transaction ? y/n ?"))
+		{
+
+			if (ClientTransferTo.Withdraw(Amount))
+
+			{
+				ClientTransferFrom.Deposit(Amount);
+
+				cout << "\n\nTransfer Done Successfully :-) :\n\n";
+
+				_PrintClientCard(ClientTransferFrom);
+
+				cout << "\n\n";
+
+				_PrintClientCard(ClientTransferTo);
+
+			}
+			else
+			{
+
+				cout << "\n\nTransfer FAILD ! Try again";
+
+			}
+
+
+		}
+		else
+		{
+			cout << "\n\nNothing Hapenned ! ";
+
+		}
+		
+
+
+	}
 
 
 
