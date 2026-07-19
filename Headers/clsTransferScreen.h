66d9dab932b1cb53487edd66ebea068220940f27
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-19 17:23:12 +0100

    Add _PrintClientCard method to clsTransferScreen class

--- Headers/clsTransferScreen.h
+++ Headers/clsTransferScreen.h
@@ -13,6 +13,16 @@ class clsTransferScreen : protected clsScreen
 
 private:
 
+	static void _PrintClientCard(const clsBankClient& Client)
+	{
+		cout << "\nClient Card :" << endl;
+		cout << "------------------------------------------------" << endl;
+		cout << "Full Name  :" << Client.GetFullName() << endl;
+		cout << "Acc.Number :" << Client.GetAccountNumber() << endl;
+		cout << "Balance    :" << Client.Balance << endl;
+		cout << "------------------------------------------------" << endl;
+
+	}
 
 
 public:
