51109b97574e818f6cfb1d4f5064d261cf51dcf2
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-20 19:46:12 +0100

    Add Currencies.txt file to BankData document file, and add clsCurrency.h file to Headers document file

---
+++ Headers/clsCurrency.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "clsString.h"
+
+using namespace std;
+
+class clsCurrency
+{
+
+private:
+
+
+
+
+public:
+
+
+
+
+};
+
+
+
