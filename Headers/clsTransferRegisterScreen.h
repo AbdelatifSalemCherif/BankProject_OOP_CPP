7c0d052bb69a718c8d0b183ed484282d88199b91
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-20 16:55:58 +0100

    Change _PrintTransferRegisterLineOfTable and ShowTransferRegisterScreen methods in clsTransferRegisterScreen class, for print the table in exact size

--- Headers/clsTransferRegisterScreen.h
+++ Headers/clsTransferRegisterScreen.h
@@ -19,13 +19,13 @@ private:
 	{
 
 		cout << setw(8) << left << "";
-		cout << "| " << left << setw(25) << TransferRegister.Time;
-		cout << "| " << left << setw(20) << TransferRegister.SourceAccountNumber;
-		cout << "| " << left << setw(20) << TransferRegister.DestinationAccountNumber;
-		cout << "| " << left << setw(7)  << TransferRegister.Amount;
-		cout << "| " << left << setw(20) << TransferRegister.SourceNewBalance;
-		cout << "| " << left << setw(20) << TransferRegister.DestinationNewBalance;
-		cout << "| " << left << setw(15)<< TransferRegister.UserName << endl;
+		cout << "| " << left << setw(22) << TransferRegister.Time;
+		cout << "| " << left << setw(17) << TransferRegister.SourceAccountNumber;
+		cout << "| " << left << setw(22) << TransferRegister.DestinationAccountNumber;
+		cout << "| " << left << setw(15) << TransferRegister.Amount;
+		cout << "| " << left << setw(18) << TransferRegister.SourceNewBalance;
+		cout << "| " << left << setw(23) << TransferRegister.DestinationNewBalance;
+		cout << "| " << left << setw(9)  << TransferRegister.UserName << endl;
 	}
 
 
@@ -45,19 +45,19 @@ public:
 
 
 		cout << setw(8) << left << "" << "--------------------------------------------------------------------------------------------"
-			"------------------------------------------- " << endl;
+			"------------------------------------------------- " << endl;
 
 		cout << setw(8) << left << "";
-		cout << "| " << left << setw(25) << "Time";
-		cout << "| " << left << setw(20) << "Source Acc.Number";
-		cout << "| " << left << setw(20) << "Destination Acc.Number";
-		cout << "| " << left << setw(7)  << "Amount";
-		cout << "| " << left << setw(20) << "Source New Balance";
-		cout << "| " << left << setw(20) << "Destination New Balance";
-		cout << "| " << left << setw(15) << "UserName" << endl;
+		cout << "| " << left << setw(22) << "Time";
+		cout << "| " << left << setw(17) << "Source Acc.Number";
+		cout << "| " << left << setw(22) << "Destination Acc.Number";
+		cout << "| " << left << setw(15)  << "Amount";
+		cout << "| " << left << setw(18) << "Source New Balance";
+		cout << "| " << left << setw(23) << "Destination New Balance";
+		cout << "| " << left << setw(9)  << "UserName" << endl;
 
 		cout << setw(8) << left << "" << "--------------------------------------------------------------------------------------------"
-			"------------------------------------------- " << endl;
+			"------------------------------------------------- " << endl;
 
 		for (clsBankClient::stTransferRegister& TransferRegister : vTransferRegister)
 		{
@@ -66,7 +66,7 @@ public:
 		}
 
 		cout << setw(8) << left << "" << "--------------------------------------------------------------------------------------------"
-			"------------------------------------------- " << endl;
+			"------------------------------------------------- " << endl;
 	}
 
 
