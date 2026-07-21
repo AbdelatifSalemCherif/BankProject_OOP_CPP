da3082d19dd828f795a0f512e0f4562d4fb69325
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-21 18:32:17 +0100

    Add enCurrencyMainMenuOptions enum to clsCurrencyMainScreen class

--- Headers/clsCurrencyMainScreen.h
+++ Headers/clsCurrencyMainScreen.h
@@ -13,7 +13,9 @@ class clsCurrencyMainScreen : protected clsScreen
 
 private:
 
-
+	enum enCurrencyMainMenuOptions {
+		eCurrenciesList = 1, eFindCurrency = 2, eUpdateCurrency = 3, eCurencyCalculator = 4, eExit = 5
+	};
 
 
 
