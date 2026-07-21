735ba45e0b81770c87a008cfc56fec64002bc4fa
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-21 19:17:31 +0100

    Add _PrintCurrencyLineOfList method to clsCurrenciesListScreen class

--- Headers/clsCurrenciesListScreen.h
+++ Headers/clsCurrenciesListScreen.h
@@ -15,7 +15,14 @@ class clsCurrenciesListScreen : protected clsScreen
 
 private:
 
-
+	static void _PrintCurrencyLineOfList(const clsCurrency& Currency)
+	{
+		cout << setw(8) << left << "";
+		cout << "| " << left << setw(15) << Currency.GetCountry();
+		cout << "| " << left << setw(20) << Currency.GetCurrencyName();
+		cout << "| " << left << setw(30) << Currency.GetCurrencyCode();
+		cout << "| " << left << setw(15) << Currency.Rate << endl;
+	}
 
 public:
 
