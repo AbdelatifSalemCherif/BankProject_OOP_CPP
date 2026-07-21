c5654ec297752591840a03880e64291ff5a50b63
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-21 20:24:35 +0100

    Add clsFindCurrencyScreen.h file to Headers document file

---
+++ Headers/clsFindCurrencyScreen.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+#include "clsCurrency.h"
+
+using namespace std;
+
+class clsFindCurrencyScreen : protected clsScreen
+{
+
+
+private:
+
+
+
+
+public:
+
+
+
+
+};
