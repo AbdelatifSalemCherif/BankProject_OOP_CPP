7bedd9a39bacdfa4a61c856250d6d5ff7ee6310c
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-21 21:38:57 +0100

    Add clsUpdateRate.h file to Headers document file

---
+++ Headers/clsUpdateRateScreen.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+#include "clsCurrency.h"
+
+using namespace std;
+
+
+class clsUpdateRateScreen : protected clsScreen
+{
+
+private:
+
+
+
+public:
+
+
+};
