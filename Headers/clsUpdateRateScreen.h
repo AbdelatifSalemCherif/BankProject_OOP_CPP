9e94d6a63501a5a8c04cf8c92283d7fb1f81fd8e
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-21 21:49:38 +0100

    Add ShowUpdateRateScreen method in clsUpdateRateScreen class

--- Headers/clsUpdateRateScreen.h
+++ Headers/clsUpdateRateScreen.h
@@ -32,14 +32,41 @@ private:
 
 
 
-
-
-
-
 public:
 
+	static void ShowUpdateRateScreen()
+	{
+
+		_DrawScreenHeader("Update Rate Screen");
 
 
+		clsCurrency Currency = clsCurrency::FindByCode(clsInputSettings::ReadString("\n\nPlease Enter Currency Cond : "));
+
+		if (Currency.IsEmpty())
+		{
+			cout << "\n\nCurrency Does Not Exist ! Try Again !\n\n";
+
+		}
+		else
+		{
+
+			_PrintCurrencyCard(Currency);
+
+
+			Currency.Rate = clsInputSettings::ReadPositiveFloat(
+				"\n\nUpdate Rate :\n------------------------------------\n\nNew Rate : ", "\n\nRate Should Be a Positive Number : ");
+
+
+			cout << "\n\nRate Updated Successfully :-)\n";
+
+			_PrintCurrencyCard(Currency);
+
+
+		}
+
+
+
+	}
 
 
 
