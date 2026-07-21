cba24d910f47f8e923f675168b4d97696b253a4d
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-21 19:14:31 +0100

    Add clsCurrenciesListScreen.h file to Headers document file

---
+++ Headers/clsCurrenciesListScreen.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+#include "clsOutputSettings.h"
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+#include "clsCurrency.h"
+
+using namespace std;
+
+class clsCurrenciesListScreen : protected clsScreen
+{
+
+private:
+
+
+
+public:
+
+
+
+
+};
