e0f6b2f2f0624f9b8d016a9619fb6965d2df8f3e
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-21 18:34:20 +0100

    Add _GoBackToCurrencyMainMenu method in clsCurrencyMainScreen class

--- Headers/clsCurrencyMainScreen.h
+++ Headers/clsCurrencyMainScreen.h
@@ -17,6 +17,21 @@ private:
 		eCurrenciesList = 1, eFindCurrency = 2, eUpdateCurrency = 3, eCurencyCalculator = 4, eExit = 5
 	};
 
+	static void _GoBackToCurrencyMainMenu() //************************
+	{
+		cout << setw(37) << left << "" << "\n\tPress any key to go back to Currency Main Menue...\n";
+
+		system("pause>0");
+
+		//ShowCurrencyMainMenu();
+	}
+
+
+
+
+
+
+
 
 
 public:
