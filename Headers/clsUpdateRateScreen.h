0f0c13e62416ce534cebfb3131051f117afe3520
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-21 21:40:07 +0100

    Add _PrintCurrencyCard method to clsUpdateRateScreen class

--- Headers/clsUpdateRateScreen.h
+++ Headers/clsUpdateRateScreen.h
@@ -15,9 +15,32 @@ class clsUpdateRateScreen : protected clsScreen
 
 private:
 
+	static void _PrintCurrencyCard(const clsCurrency& Currency)
+	{
+
+		cout << "\n\nCurrency Info :" << endl;
+
+		cout << "------------------------------------------" << endl;
+
+		cout << "Country       :" << Currency.GetCountry() << endl;
+		cout << "Currency Name :" << Currency.GetCurrencyName() << endl;
+		cout << "Currency Code :" << Currency.GetCurrencyCode() << endl;
+		cout << "Rate (1$)     :" << Currency.Rate << endl;
+
+		cout << "------------------------------------------" << endl;
+	}
+
+
+
+
+
 
 
 public:
+
+
+
+
 
 
 };
