c65252955db81ae35065a98ac10b2e1d7318bed1
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-22 17:30:03 +0100

    Add ShowCurrencyCalculatorScreen method to clsCurrencyCalculatorScreen class

--- Headers/clsCurrencyCalculatorScreen.h
+++ Headers/clsCurrencyCalculatorScreen.h
@@ -55,6 +55,77 @@ private:
 public:
 
 	
-	
+	static void ShowCurrencyCalculatorScreen()
+	{
+
+		do {
+			clsOutputSettings::RestScreen();
+
+			_DrawScreenHeader("Currency Calculator Screen");
+
+			clsCurrency Currency1 = _ReadCurrencyByCode("\n\nPlease Enter Currency 1 Code : "
+				, "\n\nInvalid Code ! Please Enter A Valid One : ");
+
+			clsCurrency Currency2 = _ReadCurrencyByCode("\n\nPlease Enter Currency 2 Code : "
+				, "\n\nInvalid Code ! Please Enter A Valid One : ");
+
+
+			float Amount = clsInputSettings::ReadPositiveFloat("\n\nPlease Enter Amount To Exchange : "
+				, "\n\nAmount Should Be A Possitive Number ! Please Enter Again : ");
+
+
+			if (Currency1.GetCurrencyCode() == Currency2.GetCurrencyCode())
+			{
+				cout << "\n\nIs The Same Currency !\n\nYour Amount Still = " << Amount << " " << Currency1.GetCurrencyCode();
+			}
+			else
+			{
+
+				cout << "\n\nConvert From :" << endl;
+
+				_PrintCurrencyCard(Currency1);
+
+				if (Currency2.GetCurrencyCode() == "USD")
+				{
+
+					cout << "\n\n" << Amount << " " << Currency1.GetCurrencyCode() << " = "
+						<< _ExchangeToUSD(Amount, Currency1.Rate) << " USD" << endl;
+
+
+				}
+				else if (Currency1.GetCurrencyCode() == "USD")
+				{
+
+					cout << "\n\n" << Amount << " USD" << " = "
+						<< _ExchangeFromUSD(Amount, Currency2.Rate) << " " << Currency2.GetCurrencyCode() << endl;
+
+				}
+				else
+				{
+
+					float AmountInUSD = _ExchangeToUSD(Amount, Currency1.Rate);
+
+					cout << "\n\n" << Amount << " " << Currency1.GetCurrencyCode() << " = "
+						<< AmountInUSD << " USD" << endl;
+
+					cout << "\n\nConverting From USD To :" << endl;
+
+					_PrintCurrencyCard(Currency2);
+
+					cout << "\n\n" << AmountInUSD << " USD" << " = "
+						<< _ExchangeFromUSD(AmountInUSD, Currency2.Rate) << " " << Currency2.GetCurrencyCode() << endl;
+
+
+				}
+
+			}
+
+
+
+
+		} while (clsInputSettings::ReadYesOrNo("\n\nDo You Want To Perform Another Calculation ? y/y ?"));
+
+	}
+
 
 };
