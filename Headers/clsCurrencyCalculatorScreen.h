fee15fd1d0a22ea397dae9f38c223c8cc709638e
Abdelatif SALEM CHERIF <abdelatif.salemcherif@gmail.com>
2026-07-22 15:38:48 +0100

    Add clsCurrencyCalculatorScreen.h file To Headers document file

---
+++ Headers/clsCurrencyCalculatorScreen.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+#include "clsInputSettings.h"
+#include "clsScreen.h"
+#include "clsCurrency.h"
+
+using namespace std;
+
+class clsCurrencyCalculatorScreen : protected clsScreen
+{
+
+private:
+
+
+public:
+
+
+
+};
